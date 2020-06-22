15d57707fea24658d3189256a6bde831cd5502e7
Prutha Kulkarni <pruthakulkarni@Pruthas-MacBook-Air.local>
2020-06-22 17:43:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+
+  console.log('a user connected');
+  socket.on("sendMessageToServer", (value)=>{
+    io.emit("messageFromServer", value);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(4000, () => {
+  console.log('listening on *:4000');
+});
