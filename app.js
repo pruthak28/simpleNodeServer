51d9112f5c7a2089d1d9ad07dddc85643ab6cde0
Prutha Kulkarni <pruthakulkarni@Pruthas-MacBook-Air.local>
2020-07-10 16:51:28 -0400

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
 
+app.use(cors);
 io.on('connection', (socket) => {
 
   console.log('a user connected');
